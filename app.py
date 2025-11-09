49cb1be1ad8268e84c693a4e084800016cd44ce2
ayushy-art <carolahono70@gmail.com>
2025-11-09 16:28:03 +0300

    Fix NameError in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/whatsapp", methods=["POST"])
 def whatsapp_reply():
@@ -18,5 +18,5 @@ def whatsapp_reply():
 
     return str(resp)
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(debug=True)
