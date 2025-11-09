e953df361fe3787c9f39d833149bb93c6981fd41
ayushy-art <carolahono70@gmail.com>
2025-11-09 15:20:06 +0300

    Add app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(_name_)
+
+@app.route("/whatsapp", methods=["POST"])
+def whatsapp_reply():
+    incoming_msg = request.values.get("Body", "").lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+
+    if "assignment" in incoming_msg:
+        msg.body("Assignment is due Friday.")
+    elif "feedback" in incoming_msg:
+        msg.body("Thanks for your feedback.")
+    else:
+        msg.body("Hey! I’m your class chatbot. Ask me about assignments, exams, or share feedback.")
+
+    return str(resp)
+
+if _name_ == "_main_":
+    app.run(debug=True)
